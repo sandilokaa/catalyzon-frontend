8297ba12e589bb7fdd4f79ca2dfd667e51718754
sandilokaa <sandyloka790@gmail.com>
2024-05-17 17:42:29 +0700

    docs: update

--- src/assets/data/solutionSwiper.js
+++ src/assets/data/solutionSwiper.js
@@ -15,7 +15,7 @@ export const SolutionsCollectionData = {
                 title: "Waste Management Service",
                 description: 'Our Waste Management Service is an integral part of our commitment to advancing sustainability in business. This service is designed to help organizations manage their waste effectively and sustainably, ensuring minimal environmental impact.',
                 alt: "Waste Management Service Image",
-                link: '/solution/waste-management-service'
+                link: '/solution/detail/1'
             }
         },
         {
@@ -27,7 +27,7 @@ export const SolutionsCollectionData = {
                 title: "Waste to Energy Project",
                 description: 'Our Waste to Energy Project is a pivotal element of our commitment to advancing sustainability in business. This project focuses on transforming waste materials into valuable energy resources, thereby reducing environmental impact and promoting resource efficiency.',
                 alt: "Waste to Energy Project Image",
-                link: '/solution/waste-to-energy-project'
+                link: '/solution/detail/2'
             }
         },
         {
@@ -39,7 +39,7 @@ export const SolutionsCollectionData = {
                 title: "Event, Training, and Community Development",
                 description: 'Our Event, Training, and Community Development Service is an essential part of our commitment to advancing sustainability in business. This service is designed to empower businesses and communities through educational initiatives, engagement events, and development programs.',
                 alt: "Training and Community Development Image",
-                link: '/solution/event-training-community-development'
+                link: '/solution/detail/3'
             }
         },
         {
@@ -51,7 +51,7 @@ export const SolutionsCollectionData = {
                 title: "Digital Decarbonization",
                 description: 'Our Digital Decarbonization Service is a critical component of our commitment to advancing sustainability in business. This service leverages cutting-edge digital technologies to help organizations reduce their carbon footprint and achieve their decarbonization goals.',
                 alt: "Environmental & Energy Consulting Image",
-                link: '/solution/digital-decarbonization'
+                link: '/solution/detail/4'
             }
         }
     ]
